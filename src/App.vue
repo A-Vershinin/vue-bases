978b7f267122e2d59e4a5ed9ecb0d609bdc9addb
ezlo-anatoliiv <anatoliy.vershynin@ezlo.com>
2019-06-14 13:42:44 +0300

    Add example how use forms inputs, checkbox, radio buttons, select, textarea

--- src/App.vue
+++ src/App.vue
@@ -3,32 +3,104 @@
 export default {
   data() {
     return {
-      searchName: '',
+      socials: ['vk'],
+      socialRadio: 'facebook',
+      socialsSelect: ['instagram', 'vk', 'facebook'],
+      selectedSocial: 'vk',
+      age: 20,
+      textareaValue: 'default text ',
     }
   },
+  watch: {
+    age(value) {
+      console.log('watch age:', value);
+    }
+  }
 };
-
 /*
- Модификатор lazy. Меняет событие, по которому меняется инпут.
- Изначальное событие 'input' при привязке через директиву v-model.
- С модификатором всплывающее событие меняется на то, какое действие было с
- инпутом, фоокус, onChange и т.д и значение инпута меняется в момент когда
- событие произошло.
- Сейчас пример - печатаем текст в инпуте, поле в фокусе, после потери фокуса,
- всплывает event 'onFocus' и тогда value инпута перезаписывается.
- Такие оптимизации лучше для скорости работы.
-*/
+  Работа с чекбоксами отличается от других фреймворков.
+  Vue использует массив с значениями от value в инпутах и смотрит есть ли такое
+  значение или нет и отмечает выбранный чекбокс. Для уставноки чекбокса
+  по-умолчанию выбранным, нужно положить сразу значение в массив.
 
+  C радиокнопками подход другой. Выбранное значение может быть только одно и это
+  передаётся из value. Поэтому в директиву привызываем только переменную для
+  хранения значения из value. Атрибут name можно не использовать. Для выбора
+  по умолчанию баттона нужно в переменную сразу положить одно из трех значение.
+
+  Селекты. Значение по умолчанию задаётся на селекте через привязку к директиве.
+  Для инпутов с числами используем модификатор в директиве. Приводит значение к числу.
+
+*/
 </script>
 
 
 
 <template>
   <div id="app">
-    <h2>Form inputs</h2>
-    <input type="text" v-model.lazy="searchName" />
+    <h2>Form inputs: checkbox</h2>
+    <label>
+      <input type="checkbox" value="instagram" v-model="socials"> Instagram
+    </label>
+
+    <label>
+      <input type="checkbox" value="vk" v-model="socials"> Vk
+    </label>
+
+    <label>
+      <input type="checkbox" value="facebook" v-model="socials"> Facebook
+    </label>
+
+    <ul>
+      <li v-for="item of socials">
+        {{ item }}
+      </li>
+    </ul>
+
+    <hr />
+
+    <h2> Form inputs: radio buttons</h2>
+    <label>
+      <input type="radio" value="instagram" v-model="socialRadio"> Instagram
+    </label>
+
+    <label>
+      <input type="radio" value="vk" v-model="socialRadio"> Vk
+    </label>
+
+    <label>
+      <input type="radio" value="facebook" v-model="socialRadio"> Facebook
+    </label>
+    <p>{{ socialRadio }}</p>
+
+    <hr />
+
+    <h2> Form inputs: selects</h2>
+    <select v-model="selectedSocial">
+      <option
+        v-for="item of socialsSelect"
+      >{{ item }}</option>
+    </select>
+
     <p>
-       {{ searchName }}
+      {{ selectedSocial }}
+    </p>
+
+    <hr />
+
+    <h2> Form inputs: input numbers </h2>
+    <input type="text" v-model.number="age">
+    <p>
+      {{ age }}
+    </p>
+
+    <hr />
+
+    <h2> Form inputs: textarea </h2>
+
+    <textarea v-model="textareaValue"></textarea>
+    <p>
+      {{ textareaValue }}
     </p>
   </div>
 </template>
