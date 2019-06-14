4948dfea898645f2583c100012f7324b0c57aa71
ezlo-anatoliiv <anatoliy.vershynin@ezlo.com>
2019-06-14 10:43:38 +0300

    Add example how work livecycle methods in custome directive

--- src/main.js
+++ src/main.js
@@ -2,13 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import ColorDerective from './color';
 
-/*
-  Пример создания своих пользовательских директив.
-  Директиру регистрируем глобально до вызова основного приложения в котором будет
-  использовать свою директиву. В методе directive указываем имя директиве и
-  объект с настройками директивы, объект.
-*/
-
 Vue.directive('colored', ColorDerective);
 
 new Vue({
