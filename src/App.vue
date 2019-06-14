eb5139f20df17e0d8e981b28f2cc3864282534c7
ezlo-anatoliiv <anatoliy.vershynin@ezlo.com>
2019-06-14 11:52:16 +0300

    Add example how to filter list with computed property

--- src/App.vue
+++ src/App.vue
@@ -5,30 +5,36 @@ export default {
   data() {
     return {
       title: 'Hello I am Vue!',
+      searchName: '',
+      names: ['Max', 'Elena', 'Igor', 'Kate'],
     }
   },
   /*
-    Фильтры (пайпы) так же как и директивы могут быть объявлены глобально и локально.
-    Фильтры  - это функции. Если в шаблоне стоит ниже такой же заголовок с таким
-    же фильтром и + другой, то знаение из одного передается в другой. Фильры
-    влияют только на отображение, они не меняют данные. Изначального нет никаких
-    фильтром, все фильтры пользовательские
+    Пример фильтрации списка. Для таких задач используется
+    computed свойство, а не фильтры, т.к. работает медленнее.
   */
+  computed: {
+    filteredNames() {
+      return this.names.filter(name => {
+        return name.toLowerCase().indexOf(this.searchName.toLowerCase()) !== -1;
+      })
+    },
+  },
   filters: {
     lowercase(value) {
       return value.toLowerCase();
     }
-  }
+  },
 };
 </script>
 
 <template>
   <div id="app">
     <h2>{{ title }}</h2>
-    <h2>{{ title | lowercase }}</h2>
-    <h2>{{ title | uppercase }}</h2>
-    <h2>{{ title | uppercase | lowercase }}</h2>
-
+    <input type="text"  v-model="searchName" />
+    <ul>
+      <li v-for="name of filteredNames">{{ name }}</li>
+    </ul>
   </div>
 </template>
 
