6eab1e1c70dffa5ed16e515fab06a62fd0df072d
ezlo-anatoliiv <anatoliy.vershynin@ezlo.com>
2019-06-18 18:06:15 +0300

    Clean structure to default

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,23 @@
-<template>
-  <div id="app">
-    <BaseExample />
-
-  </div>
-</template>
-
-<!-- <TodoList v-bind:list="groceryList" /> -->
-
 <script>
-import BaseExample from './components/BaseExample.vue';
-import TodoList from './components/TodoList.vue';
-
-const arrGrocery = [
-  { id: 0, text: 'Овощи' },
-  { id: 1, text: 'Сыр' },
-  { id: 2, text: 'Что там ещё люди едят?' }
-];
 
 export default {
   data() {
-    return {
-      groceryList: [...arrGrocery],
-    }
-  },
-  components: {
-    BaseExample,
+    return {}
   },
 }
 </script>
 
+
+<template>
+  <div id="app">
+    <div class="container">
+      <h1>Hello Vue!</h1>
+    </div>
+  </div>
+</template>
+
 <style>
 #app {
-  text-align: center;
   margin-top: 60px;
 }
 </style>
